cb6411c5e7f758d22f5a3bf949b191118554e8b6
ianbelfatto <ibelfatto@gmail.com>
2021-04-28 15:18:29 -0400

    adds employee as class

--- store_item.rb
+++ store_item.rb
@@ -1,14 +1,53 @@
-=begin
-button1 = {color: "blue", price: 500, style: "colonial"}
-button2 = {color: "red", price: 200, style: "mid-century"}
-button3 = {color: "indigo", price: 1000000, style: "futuristic"}
-=end
+# button1 = {color: "blue", price: 500, style: "colonial"}
+# button2 = {color: "red", price: 200, style: "mid-century"}
+# button3 = {color: "indigo", price: 1000000, style: "futuristic"}
+
+# button1 = {:color => "blue", :price => 500, :style => "colonial"}
+# button2 = {:color => "red", :price => 200, :style => "mid-century"}
+# button3 = {:color => "indigo", :price => 1000000, :style => "furturistic"}
+
+# puts "#{button1[:color]} #{button1[:price]} #{button1[:style]}"
+# puts "#{button2[:color]} #{button2[:price]} #{button2[:style]}"
+# puts "#{button3[:color]} #{button3[:price]} #{button3[:style]}"
 
 
-button1 = {:color => "blue", :price => 500, :style => "colonial"}
-button2 = {:color => "red", :price => 200, :style => "mid-century"}
-button3 = {:color => "indigo", :price => 1000000, :style => "furturistic"}
+class Items
+  def initialize(color, price, style, real)
+    @color = color
+    @price = price
+    @style = style
+    @real = real    
+  end
 
-puts "#{button1[:color]} #{button1[:price]} #{button1[:style]}"
-puts "#{button2[:color]} #{button2[:price]} #{button2[:style]}"
-puts "#{button3[:color]} #{button3[:price]} #{button3[:style]}"
+  # getter/reader
+
+  def color
+    @color    
+  end
+
+  def price
+    @price
+  end
+
+  def style
+    @style
+  end
+
+  def real
+    @real
+  end
+
+  # setter/writer
+  def real=(input_active)
+    @real = input_active
+  end
+end
+
+button1 = Items.new("blue", "500", "colonial", "real")
+button2 = Items.new("red", "200", "mid-century", "real")
+button3 = Items.new("indigo", "1000000", "futuristic", "real")
+puts button1.color
+puts button2.color
+puts button3.color
+button1.real = "not real"
+puts button1.real

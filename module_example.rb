cc29cb3a29055b11bfac6a79fd40ea6d5149d5da
ianbelfatto <ibelfatto@gmail.com>
2021-04-30 13:04:19 -0400

    adds module_example.rb file

---
+++ module_example.rb
@@ -0,0 +1,39 @@
+module AllVehicles
+  attr_accessor :speed, :direction
+  def initialize
+    @speed = 0
+    @direction = 'north'
+  end
+
+  def brake
+    @speed = 0
+  end
+
+  def accelerate
+    @speed += 10
+  end
+
+  def turn(new_direction)
+    @direction = new_direction
+  end
+end
+
+class Car
+  include AllVehicles
+  def honk_horn
+    puts "Beeeeeeep!"
+  end
+end
+
+class Bike
+  include AllVehicles
+  def ring_bell
+    puts "Ring ring!"
+  end
+end
+
+bike = Bike.new()
+bike.accelerate
+p bike.speed
+bike.brake
+p bike.speed
